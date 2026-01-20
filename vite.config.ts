b52bd7edf4d569a6d2f33b9343458b8097a31f64
Júnior Alves <39176934+juniorwebyte@users.noreply.github.com>
2026-01-20 14:00:57 -0300

    new updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,6 @@
-{
-  "version": 2,
-  "framework": "vite",
-  "buildCommand": "npm run build",
-  "installCommand": "npm install",
-  "outputDirectory": "dist",
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  "routes": [
-    {
-      "handle": "filesystem"
-    },
-    {
-      "src": "/.*",
-      "dest": "/index.html"
-    }
-  ],
-
-  "headers": [
-    {
-      "source": "/assets/:path*",
-      "headers": [
-        {
-          "key": "Cache-Control",
-          "value": "public, max-age=31536000, immutable"
-        }
-      ]
-    }
-  ]
-}
+export default defineConfig({
+  plugins: [react()]
+})
